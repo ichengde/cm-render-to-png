2e9c8ed16d6d2368228a1efeecb691ba103d6db2
chegde <chegde@foxmail.com>
2019-08-01 14:31:58 +0800

    chore: cast space to speed

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,58 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
-const puppeteer = require('puppeteer');
 
-/* GET home page. */
+const chrome = require('../chrome')
+
+var crypto = require('crypto')
+
 router.get('/', async function (req, res, next) {
 
-  const browser = await puppeteer.launch({ executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" });
-  const page = await browser.newPage();
+  const { url, method, data, title } = req.query;
 
+  if (!url || !data || !method) {
+    res.send('params error');
+  }
 
-  await page.setViewport({
-    width: 600,
-    height: 600,
-    deviceScaleFactor: 1,
-  });
+  const toUrl = decodeURIComponent(url + '#/' + method + '?title=' + title + '&data=' + data);
+  const page = await chrome.getPage()
 
+  let shasum = crypto.createHash('sha1');
+  shasum.update(encodeURIComponent(method + data));
+  const fileKey = './image/' + shasum.digest('hex');
+  console.log(fileKey);
 
-  const toUrl = decodeURIComponent(req.query.url + '#/' + req.query.method + '?title=' + req.query.title + '&data=' + req.query.data);
+  if (fs.existsSync(fileKey)) {
+    const img = fs.readFileSync(fileKey)
+
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': img.length
+    });
+
+    res.end(img);
+
+    return;
+  }
 
   await page.goto(toUrl);
-  const n = await page.screenshot({
-    encoding: "binary"
-  });
 
+  setTimeout(async function () {
 
-  res.writeHead(200, {
-    'Content-Type': 'image/png',
-    'Content-Length': n.length
-  });
-  res.end(n);
+    const n = await page.screenshot({
+      encoding: "binary"
+    });
 
-  await browser.close();
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': n.length
+    });
 
-  // var img = new Buffer(data, 'base64');
+    res.end(n);
+
+    //同步方法
+    fs.writeFileSync(fileKey, n);
+  }, 500)
 
 });
 
